d50edae54f1b9ef7ca5ffc33024f27b45f6435f4
Arthur-MONNET <artmon2002@gmail.com>
2023-03-06 10:08:08 +0100

    fix home twig bug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,30 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'abomistars' => [ // 22 par pages
+                ['name' => 'Acidling'],
+                ['name' => 'Blightwing'],
+                ['name' => 'Chaosquito'],
+                ['name' => 'Dreadhound'],
+                ['name' => 'Frostbite'],
+                ['name' => 'Gravetusk'],
+                ['name' => 'Hellfire'],
+                ['name' => 'Infernoid'],
+                ['name' => 'Jinxweaver'],
+                ['name' => 'Krakenshade'],
+                ['name' => 'Lunabite'],
+                ['name' => 'Magmaclaw'],
+                ['name' => 'Necrofrost'],
+                ['name' => 'Oozeblast'],
+                ['name' => 'Plaguetail'],
+                ['name' => 'Quicksand'],
+                ['name' => 'Razorbeak'],
+                ['name' => 'Shadowfang'],
+                ['name' => 'Thunderstrike'],
+                ['name' => 'Vampiric'],
+                ['name' => 'Wraithwing'],
+                ['name' => 'Xenogore']
+            ]
         ]);
     }
 }
